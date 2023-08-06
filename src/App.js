840210c6acc2a3762e3ef37fb25d047513685459
HarshDevelops <76532840+HarshDevelops@users.noreply.github.com>
2023-08-06 18:57:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.css"
+
+function App(){
+  return (
+    <div className="app">
+     <div className="main"> MAIN </div>
+     <div className="pyramid"> MONEY</div>
+    </div>
+  )
+}
+
+export default App
